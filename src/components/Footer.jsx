b907e7808af20e48fd1f6aca0f2bbb07a908d151
TACSSUKI <jk.tacssuki@gmail.com>
2023-07-05 01:26:24 +0800

    last fixes XD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
 
   return (
     <div className={`container ${themeClass}`}>
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-5 border-top">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
         <div className={`col-md-4 d-flex align-items-center ${themeClass}`}>
           <span
             className="mb-3 mb-md-0 text-body-secondary"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,26 @@ function Header() {
   const isLightTheme = theme.backgroundColor === "#c9cebd";
   const themeClass = isLightTheme ? "light-theme" : "dark-theme";
   return (
-    <header
-      className={`border-bottom lh-1 py-4 ${themeClass}`}
-      style={{
-        backgroundColor: theme.backgroundColor,
-        color: `${theme.color} !important`,
-      }}
-    >
-      <div className="row text center align-items-center">
-        <a
-          className={`blog-header-logo text-body-emphasis text-decoration-none ${themeClass}`}
-          href="/"
-          style={{ color: `${theme.color} !important` }}
-        >
-          🤸‍♂️ Beshie 🤸‍♀️ Generator 🤸
-        </a>
-        <div className="col-4 d-flex justify-content-end align-items-center"></div>
-      </div>
-    </header>
+    <div className={`container ${themeClass}`}>
+      <header
+        className={`border-bottom lh-1 py-4 ${themeClass}`}
+        style={{
+          backgroundColor: theme.backgroundColor,
+          color: `${theme.color} !important`,
+        }}
+      >
+        <div className="row text center align-items-center">
+          <a
+            className={`blog-header-logo text-body-emphasis text-decoration-none ${themeClass}`}
+            href="/"
+            style={{ color: `${theme.color} !important` }}
+          >
+            🤸‍♂️ Beshie 🤸‍♀️ Generator 🤸
+          </a>
+          <div className="col-4 d-flex justify-content-end align-items-center"></div>
+        </div>
+      </header>
+    </div>
   );
 }
 
